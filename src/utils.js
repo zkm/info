c26e38a5fff39eabe61b8e7cbf84281a07eabc9b
Zach Schneider <me@zachschneider.com>
2023-05-25 13:48:19 -0500

    email security

--- src/utils.js
+++ src/utils.js
@@ -1,44 +1,53 @@
-import content from './content.json';
-import { createElement } from './domUtils';
+import content from "./content.json";
+
+function createElement(tagName, className, text) {
+  const element = document.createElement(tagName);
+  if (className) {
+    element.className = className;
+  }
+  if (text) {
+    element.textContent = text;
+  }
+  return element;
+}
 
 export function createContentElements() {
-  const container = document.querySelector('.container');
+  const container = document.querySelector(".container");
 
   // Create content div
-  const contentDiv = createElement('div', 'content');
+  const contentDiv = createElement("div", "content");
 
   // Render welcome section
-  const welcomeTitle = createElement('h1', null, content.welcome.title);
-  const welcomeText = createElement('p', null, content.welcome.text);
+  const welcomeTitle = createElement("h3", null, content.content.welcome.title);
+  const welcomeText = createElement("p", null, content.content.welcome.text);
   contentDiv.appendChild(welcomeTitle);
   contentDiv.appendChild(welcomeText);
 
   // Render about section
-  const aboutTitle = createElement('h2', null, content.about.title);
-  const aboutText = createElement('p', null, content.about.text);
+  const aboutTitle = createElement("h3", null, content.content.about.title);
+  const aboutText = createElement("p", null, content.content.about.text);
   contentDiv.appendChild(aboutTitle);
   contentDiv.appendChild(aboutText);
 
   // Render contact section
-  const contactTitle = createElement('h3', null, content.contact.title);
+  const contactTitle = createElement("h3", null, content.content.contact.title);
 
   // Create separate elements for "Email:" and the email address
-  const emailLabel = createElement('strong', null, 'Email:');
-  const emailText = createElement('span', null, ` ${content.contact.email}`);
-  const contactEmail = createElement('p');
-  contactEmail.append(emailLabel, emailText);
+  const emailText = createElement("span", null, ` ${content.content.contact.text.replace("{email}", content.content.contact.email)}`);
+  const contactEmail = createElement("p");
+  contactEmail.append(emailText);
 
   contentDiv.appendChild(contactTitle);
   contentDiv.appendChild(contactEmail);
 
   // Render social links in contact section
-  const socialLinksList = createElement('ul');
-  content.contact.social.forEach(social => {
-    const listItem = createElement('li');
-    const link = createElement('a');
+  const socialLinksList = createElement("ul");
+  content.content.contact.social.forEach((social) => {
+    const listItem = createElement("li");
+    const link = createElement("a");
     link.href = social.url;
 
-    const icon = createElement('img', 'social-icon');
+    const icon = createElement("img", "social-icon");
     icon.src = social.icon;
 
     link.appendChild(icon);
